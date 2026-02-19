4425ba99e485208f9eb1b38999e2008549d7a15f
Karthik Bhaskar Magadi <karthik.magadi@bounteous.com>
2026-02-19 17:38:05 +0530

    Pushing Streams files onto repo, 19-02-2026

---
+++ 19-02-2026, Streams/Streams/StreamsAssignmentOne.java
@@ -0,0 +1,18 @@
+package Streams;
+
+// Find all even numbers from a list of integers
+// Find all numbers greater than 50
+// Convert add strings in a list to uppercase
+// Remove all null values from a list
+// Get the length of each string in a list
+// Find names starting with the letter 'A'
+// Find squares of all numbers
+// Filter employees with salary > 50,000
+// Get list of emails from list of employees
+// Count how many numbers are divisible by 3
+
+public class StreamsAssignmentOne {
+    public static void main(String[] args) {
+
+    }
+}
